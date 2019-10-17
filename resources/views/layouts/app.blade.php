97224937fc6a7ec3d134d7a55930d459d2944057
Shohanur Rahman <shohan.mbstu@gmail.com>
2019-10-17 19:10:08 +0600

    10-creating posts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,7 +85,7 @@
                   <div class="col-md-4">
                     <ul class="list-group">
                         <li class="list-group-item">
-                            <a href="">Post</a>
+                        <a href="{{route('posts.index')}}">Posts</a>
                         </li>
                         <li class="list-group-item">
                         <a href="{{route('categories.index')}}">Categories</a>
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="card card-default">
+    <div class="card-header">Create Post</div>
+    <div class="card-body">
+        <form action="{{route('posts.store')}}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" class="form-control" name="title" id="title">
+            </div>
+
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea id="description" class="form-control" name="description" cols="5" rows="5"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="content">Content</label>
+                <textarea id="content" class="form-control" name="content" cols="5" rows="5"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="published_at">Published At</label>
+                <input type="text" class="form-control" name="published_at" id="published_at">
+            </div>
+
+            <div class="form-group">
+                <label for="image">Image</label>
+                <input type="file" class="form-control" name="image" id="image">
+            </div>
+
+            <div class="form-group">
+                <button type='submit' class="btn btn-success">Create Post</button>
+            </div>
+
+        </form>
+    </div>
+
+</div>
+
+@endsection
